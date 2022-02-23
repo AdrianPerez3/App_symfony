660b88c3625eb348217dd4f09f383de54c22d242
AdrianPerez3 <rexinacho@gmail.com>
2022-02-23 17:48:52 +0100

    Anadimos la entidad nueva

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +14,17 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'default')]
     public function index(): Response
     {
-        $users = ["Adrian", "Roberto", "Fernando"];
+        // $users = ["Adrian", "Roberto", "Fernando"];
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setName("Adrian");
+        $user->setName("Roberto");
+        $user->setName("Jose Luis");
+
+        $entityManager->persist($user);
+        $entityManager->flush();
 
         return $this->render('default/index.html.twig', [
             'users' => $users,
